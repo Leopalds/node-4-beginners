a7271403677d8578661b9c91091070aa3d08e5cc
Pedro Leopoldo <79977884+Leopalds@users.noreply.github.com>
2023-05-19 14:23:58 -0300

    Objeto global

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-function sayHello(name){
-    console.log("Hello, " + name);
-}
+console.log() //From global variable
 
-sayHello('Pedro');
+setTimeout(); //global.setTimeout();
+clearTimeout();
+
+setInterval()
+clearInterval()
+
+var message = 'Olá, Mundo!';
+console.log(global.message); //undefined
+
+/*  São funções que no JS chamamos sobre o objeto window
+    Aqui no node chamamos pelo objeto global
+    a única diferença é que aqui, uma variável declarada não poderá ser
+    acessada pelo objeto global, pela modularização do Node.
+*/
