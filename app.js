4ba20acdbe4eb7d7878efedc04249d941153760d
Pedro Leopoldo <79977884+Leopalds@users.noreply.github.com>
2023-05-19 16:04:03 -0300

    Module wrapper function

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-// const logger = require('./logger'); 
-/* 
-    Usamos a função require para importar um arquivo, o retorno dela será justamente o atributo
-    exports do objeto module.
-
-    OBS: É IMPORTANTE que você utilize o retorno como uma constante para que não aconteça, acidentalmente,
-    de alterar o valor dessa variável.
-*/
-
-// logger.log('mensagem'); 
-/* 
-    Como importamos somente uma função, podemos trocar o retorno para em vez de retornar um obejto com todos os imports
-    Iremos importar somente a função 
-*/
-const log = require('./logger');
-
-log('mensagem');
+// ( function (exports, require, module, __filename, __dirname){ Como o node "chama" cada modulo
+    console.log(__filename); //Outros 2 argumentos que o node passa por padrão para todo modulo
+    console.log(__dirname);
+    const log = require('./logger');
+    
+    log('mensagem ');
+// })
