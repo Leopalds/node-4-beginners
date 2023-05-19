29695053b3836ca21c65d02a2a60bbcef6b4485d
Pedro Leopoldo <79977884+Leopalds@users.noreply.github.com>
2023-05-19 16:36:11 -0300

    Event arguments and arrow function

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const EventEmitter = require('events'); //Usando PascalCase, pois é uma classe
 const emitter = new EventEmitter();
 
 //Register a listener
-emitter.on('messageLogged', () => console.log('Listerner called'));
+emitter.on('messageLogged', (arg) => console.log('Listerner called', arg) );
+emitter.on('logging', (e) => console.log(e.message));
 
 // Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', {id: 1, url: 'http:://'});
 
+//Raise: logging (data: message)
+emitter.emit('logging', {message: 'Olá, Mundo!'});
