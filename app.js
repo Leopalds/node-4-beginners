dcb4e59f4455390c55e72fce07fdf10cf3757018
Pedro Leopoldo <79977884+Leopalds@users.noreply.github.com>
2023-05-19 16:31:52 -0300

    Eventos raise and emit

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 
-const fs = require('fs');
+const EventEmitter = require('events'); //Usando PascalCase, pois é uma classe
+const emitter = new EventEmitter();
 
-// fs.readdir Toda função nesse modulo tem a versão sincrona e a sincrona
+//Register a listener
+emitter.on('messageLogged', () => console.log('Listerner called'));
 
-//Na maioria dos casos é interessante utilizar a abordagem assincrona para lidar com multiplas requisições
-//Porém, como estamos estudando e queremos um rápido resultado para somente uma execução, iremos usar a acordagem sync
-// const files = fs.readdirSync('./');
+// Raise an event
+emitter.emit('messageLogged');
 
-// console.log(files);
-
-//Usando o jeito sync temos que passar uma função como parametro para fazer algo, ao finalizar a execução da função
-fs.readdir('./', function(err, files){
-    if(err) console.log('Error', err);
-    else console.log('Result', files);
-});
