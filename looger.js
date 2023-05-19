73eb01f0dff85c9fb58655dea75b1ea4aee32913
Pedro Leopoldo <79977884+Leopalds@users.noreply.github.com>
2023-05-19 15:36:28 -0300

    creating a module

---
+++ looger.js
@@ -0,0 +1,8 @@
+var url = 'http://mylogger.io/log';
+
+function log(message){
+    //Send a HTTp request
+    console.log(message);
+}
+
+module.exports.log = log; // Aqui exportamos a função log para o objeto module, assim poderemos acessá-lo em outros arquivos
